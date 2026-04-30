e914dad811107dfe23a7469b1334a5d801c3850c
Eva Dunmire <evadunmire@gmail.com>
2026-04-30 15:16:27 +0200

    autocmd on nvim resize

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('eva_highlight_yank', { clear = true }),
   callback = function()
     (vim.hl or vim.highlight).on_yank()
   end,
+})
+
+vim.api.nvim_create_autocmd('VimResized', {
+  command = 'wincmd =',
 })
